b6c3591b99181e6a42dbe39928f84a5131396102
ismail hakkı kemikli <ismail.kmkl58@gmail.com>
2025-03-05 14:36:43 +0300

    var variable  added

---
+++ veri_tipleri/var.dart
@@ -0,0 +1,13 @@
+void main (List<String> Args){
+  print(  "Hello World");
+  var a = 5;
+  var b = 3.14;
+  var c = "Hello World";
+  var d = true;
+  var e = [1,2,3,4,5];
+  var f = {"name":"ismail", "age":25};
+  print("$a, $b, $c, $d, $e, $f");
+  print("a: ${a.runtimeType}, b: ${b.runtimeType}, c: ${c.runtimeType}, d: ${d.runtimeType}, e: ${e.runtimeType}, f: ${f.runtimeType}");
+  print("a: ${a}, b: ${b}, c: ${c}, d: ${d}, e: ${e}, f: ${f}");
+
+}
